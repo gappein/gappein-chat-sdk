948d6a9fb74014964744a717075b8ef199ed828f
Himanshu Singh <hello2himanshusingh@gmail.com>
2020-09-01 03:51:06 +0530

    Added Time

--- gappein-ui/src/main/java/com/gappein/sdk/ui/chatView/viewholder/ReceiverMessageListViewHolder.kt
+++ gappein-ui/src/main/java/com/gappein/sdk/ui/chatView/viewholder/ReceiverMessageListViewHolder.kt
@@ -1,14 +1,15 @@
 package com.gappein.sdk.ui.chatView.viewholder
 
-import android.util.Log
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.gappein.sdk.model.Message
-import kotlinx.android.synthetic.main.item_sent_message.view.*
+import com.gappein.sdk.ui.util.DatesUtil
+import kotlinx.android.synthetic.main.item_received_message.view.*
 
 class ReceiverMessageListViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
 
     fun bind(message: Message, position: Int) {
-        view.sentTextMessage.text = message.message
+        view.receivedTextMessage.text = message.message
+        view.receivedTextMessageTime.text = DatesUtil.getTimeAgo(message.timeStamp)
     }
 }
---
+++ gappein-ui/src/main/java/com/gappein/sdk/ui/util/DatesUtil.kt
@@ -0,0 +1,52 @@
+package com.gappein.sdk.ui.util
+
+import android.annotation.SuppressLint
+import java.text.SimpleDateFormat
+import java.util.*
+
+object DatesUtil {
+    private const val SECOND_MILLIS = 1000
+    private const val MINUTE_MILLIS = 60 * SECOND_MILLIS
+    private const val HOUR_MILLIS = 60 * MINUTE_MILLIS
+    private const val DAY_MILLIS = 24 * HOUR_MILLIS
+
+    private fun currentDate(): Date {
+        val calendar: Calendar = Calendar.getInstance()
+        return calendar.getTime()
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    fun getTimeAgo(time: Long?): String? {
+        val sdf = SimpleDateFormat("MMM dd,yyyy HH:mm")
+        val resultdate = time?.let { Date(it) }
+        return resultdate?.let { getTimeAgo(it) }
+    }
+
+
+    fun getTimeAgo(date: Date): String? {
+        var time: Long = date.getTime()
+        if (time < 1000000000000L) {
+            time *= 1000
+        }
+        val now: Long = currentDate().getTime()
+        if (time > now || time <= 0) {
+            return "in the future"
+        }
+        val diff = now - time
+        return if (diff < MINUTE_MILLIS) {
+            "moments ago"
+        } else if (diff < 2 * MINUTE_MILLIS) {
+            "a minute ago"
+        } else if (diff < 60 * MINUTE_MILLIS) {
+            diff.div(MINUTE_MILLIS).toString() + " minutes ago"
+        } else if (diff < 2 * HOUR_MILLIS) {
+            "an hour ago"
+        } else if (diff < 24 * HOUR_MILLIS) {
+            diff.div(HOUR_MILLIS).toString() + " hours ago"
+        } else if (diff < 48 * HOUR_MILLIS) {
+            "yesterday"
+        } else {
+            diff.div(DAY_MILLIS).toString() + " days ago"
+        }
+    }
+}
