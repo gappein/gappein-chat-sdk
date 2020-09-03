1ba07a24e58677b7e7e45b1054ddf24889be8520
Himanshu Singh <hello2himanshusingh@gmail.com>
2020-09-04 02:36:57 +0530

    Added List

--- gappein-ui/src/main/java/com/gappein/sdk/ui/view/channelview/ChannelListFragment.kt
+++ gappein-ui/src/main/java/com/gappein/sdk/ui/view/channelview/ChannelListFragment.kt
@@ -1,6 +1,7 @@
 package com.gappein.sdk.ui.view.channelview
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
