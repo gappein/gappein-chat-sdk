221c319a82361e0a022ac15157e5745ee6705b0c
Himanshu Singh <hello2himanshusingh@gmail.com>
2020-09-21 01:06:24 +0530

    Added Search View

--- gappein-ui/src/main/java/com/gappein/sdk/ui/view/channelview/adapter/ChannelListAdapter.kt
+++ gappein-ui/src/main/java/com/gappein/sdk/ui/view/channelview/adapter/ChannelListAdapter.kt
@@ -7,11 +7,25 @@ import com.gappein.sdk.model.Channel
 import com.gappein.sdk.model.User
 import com.gappein.sdk.ui.R
 import com.gappein.sdk.ui.view.channelview.viewholder.ChannelListViewHolder
+import com.gappein.sdk.ui.view.channelview.viewholder.SearchViewHolder
 
-class ChannelListAdapter(private val channel: ArrayList<Channel> = arrayListOf(), private val onUserClick: (User) -> Unit, private val onChannelClick: (Channel, User) -> Unit) : RecyclerView.Adapter<ChannelListViewHolder>() {
+class ChannelListAdapter(
+    private val channel: ArrayList<Channel> = arrayListOf(),
+    private val onUserClick: (User) -> Unit,
+    private val onChannelClick: (Channel, User) -> Unit
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChannelListViewHolder {
-        return ChannelListViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_channel, parent, false))
+    companion object {
+        private const val VIEW_TYPE_USER = 0
+        private const val VIEW_TYPE_CHANNEL = 1
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if (viewType == VIEW_TYPE_USER) {
+            SearchViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_search, parent, false))
+        } else {
+            ChannelListViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_channel, parent, false))
+        }
     }
 
     fun addAll(channel: List<Channel>) {
@@ -20,10 +34,20 @@ class ChannelListAdapter(private val channel: ArrayList<Channel> = arrayListOf()
         notifyDataSetChanged()
     }
 
-    override fun onBindViewHolder(holder: ChannelListViewHolder, position: Int) {
-        holder.bind(channel[position], onUserClick, onChannelClick)
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(getItemViewType(position)){
+            VIEW_TYPE_USER ->(holder as SearchViewHolder).bind(){}
+            VIEW_TYPE_CHANNEL ->(holder as ChannelListViewHolder).bind(channel[position.minus(1)], onUserClick, onChannelClick)
+        }
     }
 
-    override fun getItemCount() = channel.count()
+    override fun getItemCount() = channel.count().plus(1)
 
+    override fun getItemViewType(position: Int): Int {
+        return if (position == 0) {
+            VIEW_TYPE_USER
+        } else {
+            VIEW_TYPE_CHANNEL
+        }
+    }
 }
