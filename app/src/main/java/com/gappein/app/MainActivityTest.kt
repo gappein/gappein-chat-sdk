17f8e09206e1fcf84f497c9cb441cbd8a2f164ad
Himanshu Singh <hello2himanshusingh@gmail.com>
2021-07-17 14:51:10 +0530

    Migrate: Version to 11

---
+++ app/src/main/java/com/gappein/app/MainActivityTest.kt
@@ -0,0 +1,11 @@
+package com.gappein.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityTest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_test)
+    }
+}
